80818cd2aa1410feadd8162c87a3d4ed3d22a0a3
Jon Fetahi <jf29441@seeu.edu.mk>
2024-05-18 10:31:06 +0200

    Eigth Commit

--- ProjectManagementSystem.UnitTests/Controller/TaskControllerTest.cs
+++ ProjectManagementSystem.UnitTests/Controller/TaskControllerTest.cs
@@ -89,6 +89,134 @@ namespace ProjectManagementSystem.UnitTests.ControllerTest
             Assert.Equal(2, model.Count());
         }
 
+        [Fact]
+        public async Task Edit_ReturnsViewResult_WhenUserIsAdmin()
+        {
+            // Arrange
+            var task = new ProjectTask
+            {
+                Id = 3,
+                Name = "Task 3",
+                Status = status.New,
+                Deadline = DateTime.Now.AddDays(1),
+                Description = "Description for Task 3",
+                Progress = 50,
+                DeveloperId = "1",
+                ManagerId = "2",
+                IsDeveloperAssigned = true,
+                IsManagerAssigned = true,
+                ProjectId = 1
+            };
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+
+            var taskViewModel = new TaskViewModel
+            {
+                Id = 3,
+                Name = "Task 3 UPDATED",
+                Status = status.New,
+                Deadline = "12.04.2023",
+                Description = "Description for Task 3 UPDATED",
+                Progress = 50,
+                DeveloperId = "1",
+                ManagerId = "2",
+                IsDeveloperAssigned = true,
+                ProjectId = 1
+            };
+
+            var currentUser = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "Joni"),
+                new Claim(ClaimTypes.Role, "Admin"),
+            }, "mock"));
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = currentUser
+                }
+            };
+
+            // Act
+            var result = _controller.Edit(taskViewModel.Id);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<TaskViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(task.Name, model.Name);
+            Assert.Equal(task.Description, model.Description);
+        }
+
+        [Fact]
+        public void Delete_ValidTask_RedirectToAction()
+        {
+            var task = new ProjectTask
+            {
+                Id = 2,
+                Name = "Task 2",
+                Status = status.New,
+                Deadline = DateTime.Now.AddDays(1),
+                Description = "Description for Task 2",
+                Progress = 50,
+                DeveloperId = "1",
+                ManagerId = "2",
+                IsDeveloperAssigned = true,
+                IsManagerAssigned = true,
+                ProjectId = 1
+            };
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+
+            var project = new Project
+            {
+                Id = 1,
+                Name = "Project 1",
+                Progress = 30,
+                ProjectManagerId = "1"
+            };
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+
+            var taskViewModel = new TaskViewModel
+            {
+                aId = 2, 
+                Name = "Task 2",
+                Status = status.New,
+                Deadline = "12.04.2023",
+                Description = "Description for Task 2",
+                Progress = 50,
+                DeveloperId = "1",
+                ManagerId = "2",
+                IsDeveloperAssigned = true,
+                ProjectId = 1
+            };
+
+            var controller = new TaskController(_context);
+
+            // Act
+            var result = controller.Delete(taskViewModel);
+
+            // Assert
+            Assert.IsType<RedirectToActionResult>(result);
+            var redirectToActionResult = (RedirectToActionResult)result;
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+
+            var taskInDb = _context.Tasks.Find(taskViewModel.Id);
+            Assert.Null(taskInDb);
+        }
+
+        [Fact]
+        public void Delete_InvalidTask_ReturnNotFound()
+        {
+            // Act
+            var result = _controller.Delete(new TaskViewModel { Id = 100 });
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
 
     }
 }
